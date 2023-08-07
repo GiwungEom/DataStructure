3bfb12595f674fc49592eed5371ef261b62dd012
Giwung <kiwung0701@gmail.com>
2023-08-07 16:29:43 +0900

    연결 리스트(Dummny) 정렬 삽입 추가 및 테스트 추가

--- app/src/main/java/com/gw/study/datastructure/list/GwLinkedList.kt
+++ app/src/main/java/com/gw/study/datastructure/list/GwLinkedList.kt
@@ -5,10 +5,13 @@ import org.jetbrains.annotations.TestOnly
 /**
  *  Head 에 Dummy 노드 연결.
  *  삽입 시 Dummy 노드로 인한 동일한 로직 구성 가능
+ *  @param compare 정렬 삽입 함수
  */
-class GwLinkedList<T> : GwList<T> {
+class GwLinkedList<T>(
+    private val compare: ((T, T) -> Int)? = null,
+) : GwList<T> {
 
-    private data class Node<T>(var data: T?, var next: Node<T>?)
+    private data class Node<T>(var data: T?, var next: Node<T>? = null)
 
     private var head: Node<T> = Node(null, null) // dummy
     private var cur: Node<T>? = null
@@ -17,10 +20,30 @@ class GwLinkedList<T> : GwList<T> {
     override var numberOfData: Int = 0
         private set
 
-    override fun insert(data: T): Boolean {
+    override fun insert(data: T): Boolean =
+        if (compare == null) {
+            insertWithoutOrder(data)
+        } else {
+            insertWithOrder(data)
+        }
+
+    private fun insertWithoutOrder(data: T): Boolean {
         val newNode = Node(data, null)
         newNode.next = head.next
         head.next = newNode
+        return true.also { numberOfData++ }
+    }
+
+    private fun insertWithOrder(data: T): Boolean {
+        val newNode = Node(data)
+        var node: Node<T>? = head
+
+        while (node?.next != null && requireNotNull(compare)(requireNotNull(node.next?.data), requireNotNull(newNode.data)) < 0) {
+            // node.next 가 newNode 보다 우선 순위가 높은 경우
+            node = node.next
+        }
+        newNode.next = node?.next
+        node?.next = newNode
         return true.also { numberOfData++ }
     }
 
--- app/src/test/java/com/gw/study/datastructure/list/LinkedListTest.kt
+++ app/src/test/java/com/gw/study/datastructure/list/LinkedListTest.kt
@@ -6,12 +6,15 @@ import org.junit.Test
 
 class LinkedListTest {
 
+    private val compare: (Int, Int) -> Int = { e1, e2 -> e1 - e2 }
     private lateinit var linkedList: GwLinkedList<Int>
+    private lateinit var linkedListOrder: GwLinkedList<Int>
     private val dataSize = 10
 
     @Before
     fun initialize() {
         linkedList = GwLinkedList()
+        linkedListOrder = GwLinkedList(compare)
         insertData()
     }
 
@@ -42,7 +45,6 @@ class LinkedListTest {
         }
     }
 
-
     @Test
     fun linkedList_remove_equalsTrue() {
         with(linkedList) {
@@ -55,6 +57,30 @@ class LinkedListTest {
                     assertEquals(i, remove())
                 }
             }
+        }
+    }
+
+
+    /** LinkedList Compare Insert Start  **/
+    private fun GwLinkedList<Int>.generateReversedData() {
+        var index = 0
+        repeat(dataSize) {
+            insert((dataSize - 1) - index++)
+        }
+    }
+
+    @Test
+    fun linkedList_insert_equalsTrue() {
+        linkedListOrder.generateReversedData()
+        assertEquals(0, linkedListOrder.first())
+    }
+
+    @Test
+    fun linkedList_search_equalsTrue() {
+        linkedListOrder.generateReversedData()
+        repeat(dataSize) {
+            if (it == 0) assertEquals(it, linkedListOrder.first())
+            else assertEquals(it, linkedListOrder.next())
         }
     }
 }
