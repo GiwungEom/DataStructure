317df16ffbc75adc0f1d9f6a1ab1101ae0433e41
Giwung <kiwung0701@gmail.com>
2023-08-08 15:16:09 +0900

    원형 연결 리스트 기반 스택 추가 및 테스트 추가

--- app/src/test/java/com/gw/study/datastructure/stack/StackTest.kt
+++ app/src/test/java/com/gw/study/datastructure/stack/StackTest.kt
@@ -11,6 +11,7 @@ class StackTest {
 
     private val stackOnArray: GwStackBasedOnArray<Int> by lazy { GwStackBasedOnArray(STACK_SIZE) }
     private val stackOnLinkedList: GwStackBasedOnLinkedList<Int> by lazy { GwStackBasedOnLinkedList() }
+    private val stackOnCircularLinkedList: GwStackBasedOnCircularLinkedList<Int> by lazy { GwStackBasedOnCircularLinkedList() }
 
     // Stack 데이터 생성 중위 표기
     private infix fun GwStack<Int>.generateData(stackSize: Int) = this.apply { repeat(stackSize) { push(it) } }
@@ -65,6 +66,35 @@ class StackTest {
     @Test
     fun stackBasedOnLinkedList_peek_equals() {
         with(stackOnLinkedList) {
+            this generateData STACK_SIZE
+            repeat(STACK_SIZE) {
+                assertEquals((STACK_SIZE - 1) - it, peek())
+                pop()
+            }
+        }
+    }
+
+    @Test
+    fun stackOnCircularLinkedList_push_equals() {
+        with(stackOnCircularLinkedList) {
+            this generateData STACK_SIZE
+            assertEquals(STACK_SIZE, numberOfData)
+        }
+    }
+
+    @Test
+    fun stackOnCircularLinkedList_pop_equals() {
+        with(stackOnCircularLinkedList) {
+            this generateData STACK_SIZE
+            repeat(STACK_SIZE) {
+                assertEquals((STACK_SIZE - 1) - it, pop())
+            }
+        }
+    }
+
+    @Test
+    fun stackOnCircularLinkedList_peek_equals() {
+        with(stackOnCircularLinkedList) {
             this generateData STACK_SIZE
             repeat(STACK_SIZE) {
                 assertEquals((STACK_SIZE - 1) - it, peek())
