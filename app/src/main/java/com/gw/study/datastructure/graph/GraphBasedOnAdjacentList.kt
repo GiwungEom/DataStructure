ddc437f90aec80a047e3f4cbeb631ff4495e53ac
Giwung <kiwung0701@gmail.com>
2023-08-21 11:15:02 +0900

    그래프 깊이 우선 탐색 추가 및 테스트 추가

--- app/src/main/java/com/gw/study/datastructure/graph/GraphBasedOnAdjacentList.kt
+++ app/src/main/java/com/gw/study/datastructure/graph/GraphBasedOnAdjacentList.kt
@@ -1,6 +1,7 @@
 package com.gw.study.datastructure.graph
 
 import com.gw.study.datastructure.list.GwLinkedList
+import com.gw.study.datastructure.stack.GwStackBasedOnArray
 
 /**
  *  인접 리스트 기반의 그래프
@@ -8,25 +9,83 @@ import com.gw.study.datastructure.list.GwLinkedList
  *  정점(Vertex)는 Int 형
  */
 class GraphBasedOnAdjacentList(
-    vertexNum: Int
+    private val vertexNum: Int
 ) {
 
     var numberOfEdge = 0
         private set
-    private val array: Array<GwLinkedList<Int>> = Array(size = vertexNum) { GwLinkedList() }
+    private val array: Array<GwLinkedList<Vertex>> = Array(size = vertexNum) { GwLinkedList { num1, num2 -> num1.ordinal - num2.ordinal } }
 
-    fun addEdge(fromVertex: Int, toVertex: Int) {
-        array[fromVertex].insert(toVertex)
-        array[toVertex].insert(fromVertex)
+    fun addEdge(fromVertex: Vertex, toVertex: Vertex) {
+        array[fromVertex.ordinal].insert(toVertex)
+        array[toVertex.ordinal].insert(fromVertex)
         numberOfEdge++
     }
 
-    fun showGraphEdgeInfo(action: (vertexInfo: Int, edgeInfo: Int) -> Unit) {
+    fun showGraphEdgeInfo(action: (vertexInfo: Vertex, edgeInfo: Vertex) -> Unit) {
         array.forEachIndexed { index, edgeList ->
-            edgeList.first()?.let { action(index, it) }
+            edgeList.first()?.let { action(Vertex.values()[index], it) }
             while(true) {
-                edgeList.next()?.let { action(index, it) } ?: break
+                edgeList.next()?.let { action(Vertex.values()[index], it) } ?: break
             }
+        }
+    }
+
+    // 깊이 우선 탐색
+    fun visitDfsGraphVertex(startVertex: Vertex, action: (vertex: Vertex) -> Unit) {
+        // 정점 방문 여부 체크
+        val vertexVisitCheck: Array<Vertex?> = Array(size = vertexNum) { null }
+        // 정점 방문 히스토리
+        val visitHistory: GwStackBasedOnArray<Vertex> = GwStackBasedOnArray(vertexNum)
+
+        var vertex = startVertex
+        var currentVertex = vertex
+        vertexVisitCheck.add(vertex).also { action(vertex) }
+        visitHistory.push(vertex)
+
+        while (array[vertex.ordinal].first()?.also { currentVertex = it } != null) {
+            if (vertexVisitCheck.find { it == currentVertex } == null) { // 방문 기록 없음
+                vertexVisitCheck.add(currentVertex)
+                visitHistory.push(currentVertex)
+                vertex = currentVertex
+                action(vertex)
+
+            } else { // 방문 기록 존재
+                var foundNewVertex = false
+                while (array[vertex.ordinal].next()?.also { currentVertex = it } != null) {
+                    if (vertexVisitCheck.find { it == currentVertex } == null) {
+                        vertexVisitCheck.add(currentVertex)
+                        visitHistory.push(currentVertex)
+                        vertex = currentVertex
+                        foundNewVertex = true
+                        action(vertex)
+                        break
+                    }
+                }
+
+                // 정점의 간선을 탐색하였고, 새로운 정점을 찾지 못한 상태
+                if (!foundNewVertex) {
+                    // 정점의 간선을 다 돌았으면 히스토리에서 이전 정점으로 pop 한다.
+                    val prevVertex = visitHistory.pop()
+                    if (prevVertex != null) {
+                        vertex = prevVertex
+                    } else {
+                        // 히스토리가 존재 하지 않는다면, 모든 정점을 탐색한 상태.
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    private fun Array<Vertex?>.add(vertex: Vertex) {
+        var index = 0
+        while (index < this.size) {
+            if (this[index] == null) {
+                this[index] = vertex
+                break
+            }
+            index++
         }
     }
 }
--- app/src/test/java/com/gw/study/datastructure/graph/GraphBasedOnAdjacentListTest.kt
+++ app/src/test/java/com/gw/study/datastructure/graph/GraphBasedOnAdjacentListTest.kt
@@ -10,7 +10,7 @@ class GraphBasedOnAdjacentListTest {
     fun graph_addEdge_equalsTrue() {
         val graph = GraphBasedOnAdjacentList(5)
         repeat(5) {
-            graph.addEdge(0, it)
+            graph.addEdge(Vertex.A, Vertex.values()[it])
         }
         assertEquals(5, graph.numberOfEdge)
     }
@@ -19,23 +19,62 @@ class GraphBasedOnAdjacentListTest {
     fun graph_showInfo_equalsTrue() {
         val vertexCount = 5
         // Pair<Vertex, Edge>
-        val result = GwQueueBasedOnLinkedList<Pair<Int, Int>>().apply {
+        val result = GwQueueBasedOnLinkedList<Pair<Vertex, Vertex>>().apply {
             repeat(vertexCount - 1) {
-                enqueue(0 to (vertexCount - 1) - it)
+                enqueue(Vertex.A to Vertex.values()[it + 1])
             }
             repeat(vertexCount - 1) {
-                enqueue(it + 1 to 0)
+                enqueue(Vertex.values()[it + 1] to Vertex.A)
             }
         }
 
         val graph = GraphBasedOnAdjacentList(vertexCount)
         repeat(vertexCount - 1) {
-            graph.addEdge(0, it + 1)
+            graph.addEdge(Vertex.A, Vertex.values()[it + 1])
         }
         graph.showGraphEdgeInfo { vertexInfo, edgeInfo ->
+            println("vertexInfo : $vertexInfo, edgeInfo : $edgeInfo")
             val resultPair = result.dequeue()
             assertEquals(resultPair.first, vertexInfo)
             assertEquals(resultPair.second, edgeInfo)
+        }
+    }
+
+    @Test
+    fun graph_dfsVisit_equalsTrue() {
+        val vertexNumber = Vertex.values().size
+        val resultList = listOf(
+            GwQueueBasedOnLinkedList<Vertex>().apply {
+                repeat(7) {
+                    enqueue(Vertex.values()[it])
+                }
+            },
+            GwQueueBasedOnLinkedList<Vertex>().apply {
+                enqueue(Vertex.C)
+                enqueue(Vertex.B)
+                enqueue(Vertex.A)
+                enqueue(Vertex.D)
+                enqueue(Vertex.E)
+                enqueue(Vertex.F)
+                enqueue(Vertex.G)
+            }
+        )
+
+        val graph = GraphBasedOnAdjacentList(vertexNumber)
+
+        graph.addEdge(Vertex.A, Vertex.B)
+        graph.addEdge(Vertex.A, Vertex.D)
+        graph.addEdge(Vertex.B, Vertex.C)
+        graph.addEdge(Vertex.D, Vertex.C)
+        graph.addEdge(Vertex.D, Vertex.E)
+        graph.addEdge(Vertex.E, Vertex.F)
+        graph.addEdge(Vertex.E, Vertex.G)
+
+        graph.visitDfsGraphVertex(Vertex.A) {
+            assertEquals(resultList[0].dequeue(), it)
+        }
+        graph.visitDfsGraphVertex(Vertex.C) {
+            assertEquals(resultList[1].dequeue(), it)
         }
     }
 }
